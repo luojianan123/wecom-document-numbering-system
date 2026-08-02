64f9e2576703ef125120af30d6010cea1f31d85f
luojianan <luojianan@local>
2026-08-02 09:35:49 +0800

    docs: add user guide and site footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,8 @@
 <template>
-  <RouterView />
+  <div class="app-shell">
+    <RouterView />
+    <footer class="app-footer">
+      Powerd by <strong>罗佳楠 2026</strong>
+    </footer>
+  </div>
 </template>
-
